aae7cc94c6fc2cbfc3477f755b20a56fb4b25c5d
hloniramaano <lehramaano@gmail.com>
2022-03-31 16:39:14 +0200

    Initial commit

--- flask-docker-app/init_db.py
+++ flask-docker-app/init_db.py
@@ -8,37 +8,22 @@ conn = psycopg2.connect(
         password=os.environ['DB_PASSWORD'],
         port=5430)
 
-# Open a cursor to perform database operations
 cur = conn.cursor()
 
-# Execute a command: this creates a new table
 cur.execute('DROP TABLE IF EXISTS lists;')
 cur.execute('CREATE TABLE lists (id serial PRIMARY KEY,'
                                  'item varchar (150) NOT NULL);'
                                  )
 
-# Insert data into the table
+# Populate the table
 
 cur.execute('INSERT INTO lists (item)'
             'VALUES (%s);',
-            ('A Tale of Two Cities',))
+            ('Apple',))
 
 cur.execute('INSERT INTO lists (item)'
             'VALUES (%s);',
-            ('Bob the builder',))
-
-cur.execute('INSERT INTO lists (item)'
-            'VALUES (%s);',
-            ('Disney',))
-
-
-# cur.execute('INSERT INTO books (title, author, pages_num, review)'
-#             'VALUES (%s, %s, %s, %s)',
-#             ('Anna Karenina',
-#              'Leo Tolstoy',
-#              864,
-#              'Another great classic!')
-#             )
+            ('Banana',))
 
 conn.commit()
 
---
+++ readme.txt
@@ -0,0 +1,14 @@
+Instructions
+
+#Flask Application#
+cd flask-docker-app
+pip install -r requirements.txt
+Install Postgresql then run commands
+psql
+create database to_do_list
+python init_db.py
+python app.py
+
+#Pyramid Application#
+cd pyramid_to_do_list
+pip install -r requirements.txt
